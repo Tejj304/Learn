63d5e87d27fbc6777ec270f6dd151fd5887ee30c
Tejj304 <tejuransing74@gmail.com>
2024-01-23 21:09:30 +0530

    first commit in Practice.java

---
+++ Practice.java
@@ -0,0 +1,9 @@
+// Git Essential for Beginners
+
+public class Practice{
+    public static void main(String[] args) {
+        System.out.println("I'm Tejju");
+        System.out.println("Git Practice | This code is just for Practice Purpose");
+        
+    }
+}
