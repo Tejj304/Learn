feb648b1776cea7b14e7947b8b744ee7bd51cae0
Tejj304 <tejuransing74@gmail.com>
2024-01-24 13:32:51 +0530

    Working on tejj branch

--- Practice.java
+++ Practice.java
@@ -2,18 +2,25 @@
 
 public class Practice{
     public static void main(String[] args) {
-        System.out.println("I'm Tejju");
+        // System.out.println("I'm Tejju");
         System.out.println("Git Practice | This code is just for Practice Purpose");
 
         // Second Commit
-        System.out.println("Hello Tejj !! This is your Second Commit !!!");
+        // System.out.println("Hello Tejj !! This is your Second Commit !!!");
 
-        //Undo Uncomiited Changes by Command >>> git checkout --
-        System.out.println("Checkout Command used to undo uncommited changes ");
+        // Undo Uncomiited Changes by Command >>> git checkout --
+        // System.out.println("Checkout Command used to undo uncommited changes ");
 
         //Undo comiited Changes by Command >>> git revert
-        System.out.println("revert Command used to undo commited changes ");
+        // System.out.println("revert Command used to undo commited changes ");
 
+
+        System.out.println("Now we are working on tejj branch !!!");
+        String fname = "Tejaswini";
+        String lname = "Ransing";
+        System.out.println("Your Name is : "+ fname + " " + lname );
+        int age = 21;
+        System.out.println("Your age is : " + age);
 
         
     }
