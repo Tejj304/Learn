21635121f73f5d6db9277b0e71f6efa57457d571
Tejj304 <tejuransing74@gmail.com>
2024-01-24 11:47:56 +0530

    This is my Second Commit git status!

--- Practice.java
+++ Practice.java
@@ -4,6 +4,9 @@ public class Practice{
     public static void main(String[] args) {
         System.out.println("I'm Tejju");
         System.out.println("Git Practice | This code is just for Practice Purpose");
+
+        // Second Commit
+        System.out.println("Hello Tejj !! This is your Second Commit !!!");
         
     }
 }
